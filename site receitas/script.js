9063f2b6d8598d7e0b48d5322f2567e9c78f8b71
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2023-10-03 22:34:50 -0300

    adicionado divs

--- site receitas/script.js
+++ site receitas/script.js
@@ -9,7 +9,8 @@ function getListaIngredientes() { }
 function preencheCatalogo() { }
 
 //var requestURL = //"receitasGauchas.json" aqui vem o conteudo do json
-const receitasGauchas = {
+const receitasGauchas = [
+  {
 
   nome: "Churrasco",
   ingredientes: [
@@ -20,11 +21,80 @@ const receitasGauchas = {
   ],
   instrucoes: "Tempere a carne com sal grosso, acenda o fogo de chão ou a churrasqueira e asse a carne até o ponto desejado. Sirva com os acompanhamentos.",
   imagem: "Churrasco.jpg"
+},
+{
+  nome: "Feijoada Gaúcha",
+  ingredientes: [
+    "Feijão preto",
+    "Carne de porco (linguiça, costela, orelha, pé)",
+    "Arroz",
+    "Couve manteiga",
+    "Farinha de mandioca",
+    "Laranja"
+  ],
+  instrucoes: "Cozinhe o feijão preto com as carnes de porco até ficarem macias. Sirva com arroz, couve manteiga refogada, farinha de mandioca e laranja em rodelas.",
+  imagem: "feijoada.jpg"
+},
+{
+  nome: "Arroz de Carreteiro",
+  ingredientes: [
+    "Carne de charque",
+    "Arroz",
+    "Cebola",
+    "Alho",
+    "Pimentão",
+    "Tomate",
+    "Temperos a gosto"
+  ],
+  instrucoes: "Corte a carne de charque em pedaços pequenos e refogue com cebola, alho, pimentão e tomate. Acrescente o arroz e cozinhe até que esteja pronto. Tempere a gosto e sirva.",
+  imagem: "carreteiro.jpg"
+},
+{
+  nome: "Cuca",
+  ingredientes: [
+    "Farinha de trigo",
+    "Açúcar",
+    "Ovos",
+    "Banha ou manteiga",
+    "Fermento biológico",
+    "Frutas (geralmente, banana ou maçã)",
+    "Canela"
+  ],
+  instrucoes: "Prepare uma massa com farinha de trigo, açúcar, ovos, banha ou manteiga e fermento biológico. Estenda a massa em uma forma, coloque as frutas por cima e polvilhe com açúcar e canela. Asse até dourar.",
+  imagem: "cuca.jpg"
 }
-//document.getElementsById("titulo").textContent = receitasGauchas.nome;
+]
 
-document.getElementById("ingredientes").textContent = receita.ingredientes.join(", ");
+let nome0 = document.getElementById('nome');
+nome0.innerHTML = receitasGauchas[0].nome;
 
-// let ingred = document.createElement("li");
-// ingred.textContent = ingredientes.textContent;
-// document.getElementById("ingredientes").appendChild(ingred);
+let img0 = document.getElementById('imagem');
+img0.src = receitasGauchas[0].imagem;
+
+let ingred0 = document.getElementById("ingredientes");
+
+for (let i = 0; i < receitasGauchas[0].ingredientes.length; i++) {
+  let linha = document.createElement("li");
+  linha.textContent = receitasGauchas[0].ingredientes[i];
+  ingred0.appendChild(linha);
+}
+
+let instr0 = document.getElementById('instrucoes');
+instr0.textContent = receitasGauchas[0].instrucoes;
+////////////////////////////////////////////
+let nome1 = document.getElementById('nome1');
+nome1.innerHTML = receitasGauchas[1].nome;
+
+let img1 = document.getElementById('imagem1');
+img1.src = receitasGauchas[1].imagem;
+
+let ingred1 = document.getElementById("ingredientes1");
+
+for (let i = 0; i < receitasGauchas[1].ingredientes.length; i++) {
+  let linha = document.createElement("li");
+  linha.textContent = receitasGauchas[1].ingredientes[i];
+  ingred1.appendChild(linha);
+}
+
+let instr1 = document.getElementById('instrucoes1');
+instr1.textContent = receitasGauchas[1].instrucoes;
