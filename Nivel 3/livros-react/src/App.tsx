122c6934c3e7850ef5335d22298a362136037516
Fernando-Abase <idelmar.souza@abase.com.br>
2023-10-25 13:07:28 -0300

    inciando routers

--- Nivel 3/livros-react/src/App.tsx
+++ Nivel 3/livros-react/src/App.tsx
@@ -14,3 +14,26 @@ function App() {
 }
 
 export default App;
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import LivroLista from './componentes/LivroLista';
+// import LivroDados from './componentes/LivroDados';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <div>
+//           <Switch>
+//             <Route path="/" element={<LivroLista />} />
+//             <Route path="/dados" element={<LivroDados />} />
+//           </Switch>
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
