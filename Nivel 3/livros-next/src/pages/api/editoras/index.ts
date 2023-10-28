069a5e116bfd515d9095e934605e20bb6435cba9
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2023-10-28 11:08:38 -0300

    28/08

--- Nivel 3/livros-next/src/pages/api/editoras/index.ts
+++ Nivel 3/livros-next/src/pages/api/editoras/index.ts
@@ -1,19 +1,19 @@
-import ControleEditora from '../../../classes/controle/ControleEditora';
-import { arrayEditoras } from '../../../classes/controle/ControleEditora';
-import { NextApiRequest, NextApiResponse } from 'next'
+import ControleEditora from "../../../classes/controle/ControleEditora";
+import { arrayEditoras } from "../../../classes/controle/ControleEditora";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const controleEditora =  new ControleEditora(arrayEditoras);
+const controleEditora = new ControleEditora(arrayEditoras);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (req.method === 'GET') {
-      const editoras = await controleEditora.getEditoras()
-      res.status(200).json(editoras)
+    if (req.method === "GET") {
+      const editoras = await controleEditora.getEditoras();
+      res.status(200).json(editoras);
     } else {
-      res.status(405).end() // Método não permitido
+      res.status(405).end();
     }
   } catch (error) {
     console.error(error);
-    res.status(500).end() // Erro interno no servidor
+    res.status(500).end();
   }
-}
+};
