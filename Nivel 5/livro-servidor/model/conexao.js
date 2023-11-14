71f6c43e1971cedf29c50f5e11262a2815818e2e
Fernando-Abase <idelmar.souza@abase.com.br>
2023-11-14 12:19:34 -0300

    model

---
+++ Nivel 5/livro-servidor/model/conexao.js
@@ -0,0 +1,12 @@
+const banco = require("mongoose");
+const url=  "mongodb+srv://fernandoIfs:PrBruoiYg9tPhnPh@cluster0.g5zvbo1.mongodb.net/?retryWrites=true&w=majority";
+
+banco.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Conectado com sucesso!");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar:", error);
+  });
+
+module.exports = { banco, url };
