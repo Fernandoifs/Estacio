f47f60b852a7977ae47f0b7ea401b6e356e8886d
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2023-11-13 21:27:02 -0300

    new react

--- Nivel 5/livro-servidor/modelo/conexao.js
+++ Nivel 5/livro-servidor/modelo/conexao.js
@@ -1,5 +1,6 @@
 const banco = require("mongoose");
 const url=  "mongodb+srv://fernandoIfs:PrBruoiYg9tPhnPh@cluster0.g5zvbo1.mongodb.net/?retryWrites=true&w=majority";
+//mongodb://localhost:27017/
 
 banco.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
