67401de456cf1255116474a78b6a53182d894956
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2023-11-21 12:49:30 -0300

    adicionado banco local

--- Nivel 5/livro-servidor/model/conexao.js
+++ Nivel 5/livro-servidor/model/conexao.js
@@ -1,7 +1,6 @@
 const banco = require("mongoose");
-const url= 
-//"mongodb://localhost:27017/livraria"
- "mongodb+srv://fernandoIfs:PrBruoiYg9tPhnPh@cluster0.g5zvbo1.mongodb.net/?retryWrites=true&w=majority";
+const url= "mongodb://127.0.0.1:27017/livraria";
+ //"mongodb+srv://fernandoIfs:PrBruoiYg9tPhnPh@cluster0.g5zvbo1.mongodb.net/?retryWrites=true&w=majority";
 
 banco.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
